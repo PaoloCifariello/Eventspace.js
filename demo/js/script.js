4130013f70d3ce7afbacebca8e207a1147c25749
Paolo Cifariello <paolocifa@gmail.com>
2015-02-21 15:51:35 +0100

    utilizzo di jQuery

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,8 +1,4 @@
-var but = document.getElementById('but');
 
-ES.add(['s1', 's2', 's3']);
-
-ES.on({node: but, event: 'click', space: ['s1', 's2']}, function(){alert('s1 o s2');});
-ES.on({node: but, event: 'click', space: ['s3']}, function(){alert('s3');});
-
-ES.set('s1');
+$('#but').son('click',['s1','s2'], function(){
+	console.log(this);
+});
